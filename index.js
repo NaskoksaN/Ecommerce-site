f23f87f91054ec7f1c574070b763b9c70df00e79
NaskoksaN <136219696+NaskoksaN@users.noreply.github.com>
2024-08-29 08:27:33 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const prevButton = document.querySelector('.carousel-control-prev');
 const nextButton = document.querySelector('.carousel-control-next');
 let currentIndex = 0;
 
-// Показване на първия елемент по подразбиране
+
 items[currentIndex].classList.add('active');
 
-// Функция за смяна на активния елемент
+
 function showItem(index) {
     items[currentIndex].classList.remove('active');
-    currentIndex = (index + items.length) % items.length; // Циклиране през елементите
+    currentIndex = (index + items.length) % items.length; 
     items[currentIndex].classList.add('active');
 }
 
-// Действие при натискане на бутона за предишен елемент
+
 prevButton.addEventListener('click', () => {
     showItem(currentIndex - 1);
 });
 
-// Действие при натискане на бутона за следващ елемент
+
 nextButton.addEventListener('click', () => {
     showItem(currentIndex + 1);
 });
